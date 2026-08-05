c10fa25ba3c270fff12342d4175275c27705ace2
coding_babo <kwondevelop@gmail.com>
2026-08-05 13:12:15 +0900

    edt responsecode add startWith SCG

--- src/main/java/com/meerkatgramv2scg/global/errors/GlobalErrorWebExceptionHandler.java
+++ src/main/java/com/meerkatgramv2scg/global/errors/GlobalErrorWebExceptionHandler.java
@@ -29,7 +29,7 @@ public class GlobalErrorWebExceptionHandler implements WebExceptionHandler {
     CustomResponseCode customResponseCode =
       (ex instanceof ResponseStatusException responseStatusException
       && responseStatusException.getStatusCode().value() == 404)
-      ? CustomResponseCode.NOT_FOUND_ERROR : CustomResponseCode.SYSTEM_ERROR;
+      ? CustomResponseCode.SCG_NOT_FOUND_ERROR : CustomResponseCode.SCG_SYSTEM_ERROR;
 
       response.setStatusCode(customResponseCode.getHttpStatus()); // Http Status 변경
       response.getHeaders().setContentType(MediaType.APPLICATION_JSON); // Content-Type을 Json으로 변경
